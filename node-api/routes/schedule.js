c1d4cd50efcf60c6cde6f27370d8ab220e0fca0f
hberiwal <63370699+hberiwal@users.noreply.github.com>
2020-11-15 16:22:41 +0530

    Create schedule.js

---
+++ node-api/routes/schedule.js
@@ -0,0 +1,11 @@
+// imports
+const express = require('express');
+const { model } = require('mongoose');
+
+// router init
+const router = express.Router();
+
+
+//router.get('', (req,res,next) => {
+
+//})
